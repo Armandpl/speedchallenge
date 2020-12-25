6a05de1aa11b1d9a9b36037c19a6667e86611793
paperspace <adpl33@gmail.com>
2020-12-25 06:30:49 -0500

    add test set

--- download_dataset.sh
+++ download_dataset.sh
@@ -1,5 +1,7 @@
 mkdir data
 curl -LJO https://github.com/commaai/speedchallenge/blob/master/data/train.mp4?raw=true
 curl -LJO https://github.com/commaai/speedchallenge/blob/master/data/train.txt?raw=true
+curl -LJO https://github.com/commaai/speedchallenge/blob/master/data/test.mp4?raw=true
 mv train.mp4?raw=true data/train.mp4
+mv test.mp4?raw=true data/test.mp4
 mv train.txt?raw=true data/train.txt
