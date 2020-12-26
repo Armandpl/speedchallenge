d21a7a522320c09106b6727662104a78647e9f6b
Armandpl <adpl33@gmail.com>
2020-12-26 08:17:56 +0100

    create script to plot speed over time

---
+++ plot_speed.py
@@ -0,0 +1,24 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+if __name__ == "__main__":
+    with open("data/train.txt", "r") as f:
+        lines = f.readlines()
+
+    labels = []
+    for l in lines:
+        labels.append(float(l))
+
+    print(labels)
+    t = np.zeros(len(labels))
+    labels =  np.array(labels , dtype=float)
+
+    fig, ax = plt.subplots()
+
+    ax.plot(labels)
+    ax.set(xlabel='time', ylabel='speed (mph)',
+       title='train.mp4')
+
+    fig.savefig("speed.png")
+    plt.show()
